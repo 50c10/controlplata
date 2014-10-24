2b9c010402aecb6a95e74201a0d99da45070c921
50c10 <cit_java@hotmail.com>
2014-10-24 01:52:02 -0500

    Modificacion el la pantalla que lista las solisitudes

--- apps/solisitudes/urls.py
+++ apps/solisitudes/urls.py
@@ -1,8 +1,11 @@
-from django.conf.urls import url
+from django.conf.urls import patterns, url
 
 from apps.solisitudes import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^list_solisitudes/$',views.list_solisitud, name='lista_solisitudes'),
+	url(r'^$', views.solisitud_list, name='index'),
+	url(r'^nueva$', views.solisitud_create, name='nueva_solisitud'),
+	url(r'^editar/(?P<pk>\d+)$', views.solisitud_update, name='editar_solisitud'),
+	url(r'^borrar/(?P<pk>\d+)$', views.solisitud_delete, name='borrar_solisitud'),
+	
 ]
--- apps/solisitudes/views.py
+++ apps/solisitudes/views.py
@@ -1,14 +1,37 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
+from django.shortcuts import render, redirect, get_object_or_404
+from django.forms import ModelForm
 
 from apps.solisitudes.models import Solisitud
 # Create your views here.
 
-def index(request):
-    return render(request, 'solisitudes/solisitud.html')
+class SolisitudForm(ModelForm):
+    class Meta:
+        model = Solisitud
 
-def list_solisitud(request):
-	solisitudes_list = Solisitud.objects.all()
-	context = {'lista_solisitudes': solisitudes_list}
-	return render(request, 'solisitudes/solisitud_list.html',context)
+def solisitud_list(request, template_name='solisitudes/solisitud_list.html'):
+    solisitudes = Solisitud.objects.all()
+    data = {}
+    data['object_list'] = solisitudes
+    return render(request, template_name, data)
+
+def solisitud_create(request, template_name='solisitudes/nuevo.html'):
+    form = SolisitudForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return redirect('index')
+    return render(request, template_name, {'form':form})
+
+def solisitud_update(request, pk, template_name='solisitudes/nuevo.html'):
+    solisitud = get_object_or_404(Solisitud, pk=pk)
+    form = SolisitudForm(request.POST or None, instance=solisitud)
+    if form.is_valid():
+        form.save()
+        return redirect('index')
+    return render(request, template_name, {'form':form})
+
+def solisitud_delete(request, pk, template_name='solisitudes/solisitud_borrar.html'):
+    solisitud = get_object_or_404(Solisitud, pk=pk)    
+    if request.method=='POST':
+        solisitud.delete()
+        return redirect('index')
+    return render(request, template_name, {'object':solisitud})
